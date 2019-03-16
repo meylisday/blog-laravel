3a53c144c7cf752afadfc19c75ee5cc5e087c168
meylisday <victory.sett@gmail.com>
2019-03-16 19:13:08 +0300

    changed auth links

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -20,7 +20,10 @@
             <a class="nav-link" href="{{url('contact')}}">Contact</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" href="{{url('login')}}">Sign In</a>
+            <a class="nav-link active" href="{{url('login')}}">Log in</a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link" href="{{url('register')}}">Sign in</a>
           </li>
         </ul>
       </div>
